66cf92334bf79483476df0abf74fe93656965581
jamshaid <jam172002@gmail.com>
2023-08-27 21:40:04 +0500

    Added PopUP Menu for delete and update

--- app/src/main/java/com/example/roomlib_ii/Adapter.java
+++ app/src/main/java/com/example/roomlib_ii/Adapter.java
@@ -1,10 +1,13 @@
 package com.example.roomlib_ii;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,11 +35,46 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.id.setText(String.valueOf(data.get(position).getId()));
         holder.fName.setText(data.get(position).getfName());
         holder.lName.setText(data.get(position).getlName());
         holder.Grade.setText(data.get(position).getGrade());
+
+
+        holder.option.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(context, holder.option);
+                popupMenu.inflate(R.menu.my_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+
+                        int id = menuItem.getItemId();
+                        if (id == R.id.update){
+                            Toast.makeText(context, "Updated", Toast.LENGTH_SHORT).show();
+                            return true;
+
+                        } else if (id == R.id.delete) {
+                            db = DBHelper.getDB(context);
+                            db.dao().deleteById(data.get(position).getId());
+
+                            data.remove(position);
+                            notifyDataSetChanged();
+                            return true;
+
+
+                        }
+                        return false;
+                    }
+
+                });
+                popupMenu.show();
+
+
+            }
+        });
 
         //to perform the delete operation via recycler view
        /* holder.btnDel.setOnClickListener(new View.OnClickListener() {
